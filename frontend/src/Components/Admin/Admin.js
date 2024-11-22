e0cc105fd8ca8dbc9a412cb9ae13b0687b2a9f8e
Yeabtsega Tesfaye <yeabtesfayetesfaye519@gmail.com>
2024-11-22 23:56:37 +0300

    changed the uploaded data storing method to database.

--- frontend/src/Components/Admin/Admin.js
+++ frontend/src/Components/Admin/Admin.js
@@ -174,7 +174,7 @@ const handlePrint = (course) => {
               <td>{submission.course}</td>
               <td>{submission.description}</td>
               <td>
-                <a href={`/${submission.file_path}`} target="_blank" rel="noopener noreferrer">
+                <a href={`/uploads/exercises/${submission.file_name}`} target="_blank" rel="noopener noreferrer">
                   Download
                 </a>
               </td>
